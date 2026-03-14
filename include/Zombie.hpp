61f3871235d1e5de001f34421635a4165ef94917
Sally Lin <LS0723000@gmail.com>
2026-03-15 00:04:15 +0800

    feat(zombie): add eating and damage state transitions

--- include/Zombie.hpp
+++ include/Zombie.hpp
@@ -24,9 +24,44 @@ public:
         if (m_State == State::WALK) {
             m_Transform.translation.x -= m_Speed;
         }
+        if (m_State == State::EAT) {
+            ++m_EatTimer;
+        }
+    }
+
+    void TakeDamage(int damage) {
+        m_HP -= damage;
+        if (m_HP <= 0) {
+            m_State = State::DIE;
+            SetVisible(false);
+        }
+    }
+
+    void StartEat() {
+        if (m_State != State::EAT) {
+            m_State = State::EAT;
+            m_EatTimer = 0;
+        }
+    }
+
+    void StartWalk() {
+        if (m_State != State::WALK) {
+            m_State = State::WALK;
+        }
+    }
+
+    bool ShouldDealEatDamage() {
+        if (m_State != State::EAT) return false;
+        if (m_EatTimer >= EAT_INTERVAL) {
+            m_EatTimer = 0;
+            return true;
+        }
+        return false;
     }
 
     bool IsAlive() const { return m_State != State::DIE; }
+    bool IsDead() const { return m_State == State::DIE; }
+    bool IsEating() const { return m_State == State::EAT; }
     bool IsPastLine(float x) const { return m_Transform.translation.x < x; }
 
     int GetRow() const { return m_Row; }
@@ -38,6 +73,8 @@ protected:
     float m_Speed;
     int m_HP;
     State m_State = State::WALK;
+    int m_EatTimer = 0;
+    static constexpr int EAT_INTERVAL = 30;
 };
 
 #endif // ZOMBIE_HPP
